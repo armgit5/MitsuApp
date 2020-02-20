308873617c369ff074b45e1fa1d29f74a92bb42f
Arm Suwansiri <ysuwansiri@yahoo.com>
2020-02-20 16:38:29 +0700

    finished v2

--- helpers/process.js
+++ helpers/process.js
@@ -12,7 +12,7 @@ conn.initiateConnection({port: 1281, host: '192.168.0.100', ascii: false}, conne
 
 function addAllItems() {
     for (const key in machine) {
-        if (key != 'notRealRegisters') {
+        if (key != 'commChannels') {
             conn.addItems(machine[key]);
         }
     }
@@ -40,97 +40,59 @@ function valuesWritten(anythingBad) {
 module.exports = (mainWindow) => {
     console.log('staring process');
 
-    // Starting the machine
-    ipcMain.on(machine.start, (e, isOn) => {
-        doneReading = false;
-        doneWriting = false;
-        console.log('start ', isOn);
-
-        writeHelper(conn, machine.start, 1)
-            .then(register => {
-                return writeHelper(conn, machine.start, 0);
+    // Reverse
+    ipcMain.on(machine.commChannels.reverse, (e, tick) => {
+        console.log('reverse ', tick);
+        writeHelper(conn, machine.reverse, 1)
+            .then(_ => {
+                return writeHelper(conn, machine.reverse, 0);
             })
-            .then(register => {
+            .then(_ => {
             });
-        
-        subscribeRegisters(conn, machine.unwindSpeed);
+    });
+
+    // Speed down
+    ipcMain.on(machine.commChannels.speedDown, (e, tick) => {
+        console.log('speed down ', tick);
+        writeHelper(conn, machine.speedDown, 1)
+            .then(_ => {
+                return writeHelper(conn, machine.speedDown, 0);
+            })
+            .then(_ => {
+            });
     });
 
     // Stopping the machine
-    ipcMain.on(machine.stop, (e, isOff) => {
+    ipcMain.on(machine.commChannels.stop, (e, isOff) => {
         console.log('stop ', isOff);
         writeHelper(conn, machine.stop, 1)
-            .then(register => {
+            .then(_ => {
                 return writeHelper(conn, machine.stop, 0);
             })
-            .then(register => {
+            .then(_ => {
             });
     });
 
-    ipcMain.on(machine.notRealRegisters.speed, (e, speed) => {
-        MACHINE_SPEED = speed;
-        conn.writeItems(machine.unwindSpeed, MACHINE_SPEED, valuesWritten);
-        writeHelper(conn, machine.unwindSpeed, MACHINE_SPEED).then(register => {
-            console.log('write to ', register);
-        });
-    });
-
-    ipcMain.on(machine.notRealRegisters.speedUp, (e, isSet) => {
-        console.log('speed up');
-
-        let doneWriting;
-        MACHINE_SPEED += 100;
-
-        if (doneWriting === undefined || doneWriting) {
-            doneWriting = false;
-            writeHelper(conn, machine.unwindSpeed, MACHINE_SPEED).then(register => {
-                console.log('write to ', register, MACHINE_SPEED);
-                doneWriting = true;
-            });
-        }
-    });
-
-    ipcMain.on(machine.notRealRegisters.speedUpVal, (e, val) => {
-        console.log('speed up val ', val);
-
-        let doneWriting;
-        let doneWriting2;
-
-        if (doneWriting === undefined || doneWriting) {
-            doneWriting = false;
-            writeHelper(conn, machine.unwindSpeed, val).then(register => {
-                console.log('write to ', register, val);
-                // doneWriting = true;
-                return writeHelper(conn, machine.rewindSpeed, val);
+    // Speed down
+    ipcMain.on(machine.commChannels.speedUp, (e, tick) => {
+        console.log('speed up ', tick);
+        writeHelper(conn, machine.speedUp, 1)
+            .then(_ => {
+                return writeHelper(conn, machine.speedUp, 0);
             })
-            .then(register => {
-                console.log('write to ', register, val);
-                doneWriting = true;
+            .then(_ => {
             });
-        }
-
-        if (doneWriting2 === undefined || doneWriting2) {
-            doneWriting2 = false;
-            
-        }
     });
 
-    ipcMain.on(machine.notRealRegisters.speedDown, (e, isSet) => {
-        console.log('speed down');
-
-        let doneWriting;
-        MACHINE_SPEED -= 100;
-
-        if (doneWriting === undefined || doneWriting) {
-            doneWriting = false;
-            writeHelper(conn, machine.unwindSpeed, MACHINE_SPEED).then(register => {
-                console.log('write to ', register, MACHINE_SPEED);
-                doneWriting = true;
+    // Forward
+    ipcMain.on(machine.commChannels.forward, (e, tick) => {
+        console.log('speed up ', tick);
+        writeHelper(conn, machine.forward, 1)
+            .then(_ => {
+                return writeHelper(conn, machine.forward, 0);
+            })
+            .then(_ => {
             });
-        }
     });
-    
-    ipcMain.on(machine.notRealRegisters.torque, (e, torque) => {
-        conn.writeItems(machine.unwindTorque, torque, valuesWritten);
-    });
+
 };
